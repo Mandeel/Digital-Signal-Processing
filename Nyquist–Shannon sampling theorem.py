7080511b57eb507fe381ee9e62a58e9dc6d8dff4
Mandeel <36323672+Mandeel@users.noreply.github.com>
2019-10-23 22:15:22 +0300

    Update Nyquist–Shannon sampling theorem.py

--- Nyquist–Shannon sampling theorem.py
+++ Nyquist–Shannon sampling theorem.py
@@ -19,7 +19,7 @@ tmin = -0.3;
 tmax = 0.3;
 t = linspace(tmin, tmax, 400);
 signal_a = cos(2*pi*t);
-signal_b = + cos(2*pi*f*t);
+signal_b = cos(2*pi*f*t);
 x = signal_a + signal_b; # signal sampling
 plot(t, x)
 
@@ -33,7 +33,7 @@ x1 = cos(2*pi*n*T) + cos(2*pi*f*n*T);
 plot(n*T, x1, 'bo')
 
 # sampling the signal with a sampling rate of 35 Hz
-# note that 35 Hz is under the Nyquist rate.
+# note that 35 Hz is under the Nyquist rate. (Aliasing)
 T = 1/35.0;
 nmin = ceil(tmin / T);
 nmax = floor(tmax / T);
